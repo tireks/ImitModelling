6dba4d4c0d71aa77a32589c4249e745312af13e4
tireks <33616425+tireks@users.noreply.github.com>
2024-03-07 13:05:22 +0700

    done

---
+++ Lab3_Leonov/Lab3_Leonov/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Lab3_Leonov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.editRule = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.editRule)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.Location = new System.Drawing.Point(188, -2);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(606, 454);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.editRule);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(7, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(175, 454);
+            this.panel1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(6, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Старт/Стоп";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // editRule
+            // 
+            this.editRule.Location = new System.Drawing.Point(83, 30);
+            this.editRule.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.editRule.Name = "editRule";
+            this.editRule.Size = new System.Drawing.Size(79, 20);
+            this.editRule.TabIndex = 1;
+            this.editRule.Value = new decimal(new int[] {
+            110,
+            0,
+            0,
+            0});
+            this.editRule.ValueChanged += new System.EventHandler(this.editRule_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Правило";
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.editRule)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown editRule;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Lab3_Leonov/Lab3_Leonov/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3_Leonov
+{
+    public partial class Form1 : Form
+    {
+
+        private int rows = 1, columns = 15; // начальное кол-во строк и столбцов
+        private char[] rule = new char[8];
+        private string[] Rules = new string[] {
+            "111",
+            "110",
+            "101",
+            "100",
+            "011",
+            "010",
+            "001",
+            "000"
+        };
+
+        private bool edit = true;
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < columns; i++) dataGridView1.Columns.Add("", "");
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows[0].HeaderCell.Value = rows.ToString();
+            initializerule(Convert.ToString((int)editRule.Value, 2));
+        }
+
+        private void initializerule(string pravilo)
+        {
+            int l = pravilo.Length;
+            while (l < 8)
+            {
+                pravilo = "0" + pravilo;
+                l = pravilo.Length;
+            }
+            for (int i = 0; i < pravilo.Length; i++)
+            {
+                rule[i] = pravilo[i];
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = !timer.Enabled;
+            if (timer.Enabled == false) edit = false;
+        }
+
+        private void editRule_ValueChanged(object sender, EventArgs e)
+        {
+            initializerule(Convert.ToString((int)editRule.Value, 2));
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (edit)
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    dataGridView1[e.ColumnIndex, e.RowIndex].Style.BackColor = Color.Pink;
+                    dataGridView1.ClearSelection();
+                }
+                else
+                {
+                    dataGridView1[e.ColumnIndex, e.RowIndex].Style.BackColor = Color.White;
+                    dataGridView1.ClearSelection();
+                }
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            char[] oldRow = new char[columns];
+            char[] newRow = new char[columns];
+            for (int i = 0; i < columns; i++)
+            {
+                if (dataGridView1[i, rows - 1].Style.BackColor == Color.Pink) oldRow[i] = '1';
+                else oldRow[i] = '0';
+            }
+            for (int i = 0; i < columns; i++)
+            {
+                string Rule;
+                int previousNum = i - 1;
+                int nextNum = i + 1;
+                var builder = new StringBuilder();
+
+                if (i == 0)
+                {
+                    previousNum = columns - 1;
+                }
+                if (i == columns - 1)
+                {
+                    nextNum = 0;
+                }
+
+                builder.Append(oldRow[previousNum]);  //Добавляет строковое представление указанного построителя строки к данному экземпляру.
+                builder.Append(oldRow[i]);
+                builder.Append(oldRow[nextNum]);
+
+                Rule = builder.ToString();
+                int index = Array.IndexOf(Rules, Rule);
+                newRow[i] = rule[index];
+            }
+
+            rows++;
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows[rows - 1].HeaderCell.Value = rows.ToString();
+
+            for (int i = 0; i < columns; i++)
+            {
+                if (newRow[i] == '1') dataGridView1[i, rows - 1].Style.BackColor = Color.Pink;
+                else dataGridView1[i, rows - 1].Style.BackColor = Color.White;
+            }
+        }
+    }
+}
