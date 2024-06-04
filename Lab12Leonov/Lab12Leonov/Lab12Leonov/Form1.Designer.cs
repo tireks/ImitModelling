25173642e1d4667c6e2825fbac70d206e51ca545
tireks <33616425+tireks@users.noreply.github.com>
2024-06-04 19:33:19 +0700

    Lab12Done

---
+++ Lab12Leonov/Lab12Leonov/Lab12Leonov/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Lab12Leonov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbRainy = new System.Windows.Forms.Label();
+            this.lbCloudy = new System.Windows.Forms.Label();
+            this.lbClear = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbWeather = new System.Windows.Forms.Label();
+            this.lbDay = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbRainy
+            // 
+            this.lbRainy.AutoSize = true;
+            this.lbRainy.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbRainy.Location = new System.Drawing.Point(569, 216);
+            this.lbRainy.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbRainy.Name = "lbRainy";
+            this.lbRainy.Size = new System.Drawing.Size(26, 29);
+            this.lbRainy.TabIndex = 23;
+            this.lbRainy.Text = "0";
+            // 
+            // lbCloudy
+            // 
+            this.lbCloudy.AutoSize = true;
+            this.lbCloudy.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbCloudy.Location = new System.Drawing.Point(569, 144);
+            this.lbCloudy.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbCloudy.Name = "lbCloudy";
+            this.lbCloudy.Size = new System.Drawing.Size(26, 29);
+            this.lbCloudy.TabIndex = 22;
+            this.lbCloudy.Text = "0";
+            // 
+            // lbClear
+            // 
+            this.lbClear.AutoSize = true;
+            this.lbClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbClear.Location = new System.Drawing.Point(569, 70);
+            this.lbClear.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbClear.Name = "lbClear";
+            this.lbClear.Size = new System.Drawing.Size(26, 29);
+            this.lbClear.TabIndex = 21;
+            this.lbClear.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(383, 216);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 29);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Дождь";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(383, 144);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 29);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Облачно";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(383, 70);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 29);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Ясно";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(471, 14);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 29);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Аналитика";
+            // 
+            // lbWeather
+            // 
+            this.lbWeather.AutoSize = true;
+            this.lbWeather.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbWeather.Location = new System.Drawing.Point(159, 216);
+            this.lbWeather.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbWeather.Name = "lbWeather";
+            this.lbWeather.Size = new System.Drawing.Size(26, 29);
+            this.lbWeather.TabIndex = 16;
+            this.lbWeather.Text = "0";
+            // 
+            // lbDay
+            // 
+            this.lbDay.AutoSize = true;
+            this.lbDay.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbDay.Location = new System.Drawing.Point(159, 144);
+            this.lbDay.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbDay.Name = "lbDay";
+            this.lbDay.Size = new System.Drawing.Size(26, 29);
+            this.lbDay.TabIndex = 15;
+            this.lbDay.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(23, 216);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 29);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Погода";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(23, 144);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 29);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "День";
+            // 
+            // btn
+            // 
+            this.btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn.Location = new System.Drawing.Point(27, 14);
+            this.btn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(302, 83);
+            this.btn.TabIndex = 12;
+            this.btn.Text = "Старт";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbRainy);
+            this.Controls.Add(this.lbCloudy);
+            this.Controls.Add(this.lbClear);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbWeather);
+            this.Controls.Add(this.lbDay);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbRainy;
+        private System.Windows.Forms.Label lbCloudy;
+        private System.Windows.Forms.Label lbClear;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbWeather;
+        private System.Windows.Forms.Label lbDay;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Lab12Leonov/Lab12Leonov/Lab12Leonov/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab12Leonov
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            model = new Model();
+            InitFormAndModel();
+        }
+
+        private void Start()
+        {
+            InitFormAndModel();
+            btn.Text = "Пауза";
+            timer1.Enabled = true;
+        }
+
+        private void Pause()
+        {
+            btn.Text = "Продолжить";
+            timer1.Enabled = false;
+        }
+
+        private void Continue()
+        {
+            btn.Text = "Пауза";
+            timer1.Enabled = true;
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            switch (button.Text)
+            {
+                case "Старт":
+                    Start();
+                    break;
+                case "Продолжить":
+                    Continue();
+                    break;
+                case "Пауза":
+                    Pause();
+                    break;
+            }
+        }
+
+        public class Model
+        {
+            private double[][] Q = {new double[]{-0.5, 0, 0.65, 0.35},
+                                    new double[]{-0.7, 0.2, 0, 0.8 },
+                                    new double[]{-0.4, 0.45, 0.55, 0 }};
+            private Random rand = new Random();
+            public List<Weather> stat; public List<double> time;
+            public double[] state_time;
+            public double t, dt = 1;
+
+            public void Init()
+            {
+                t = 0;
+                stat = new List<Weather> { Weather.clear };
+                time = new List<double> { 0 };
+                state_time = new double[3] { 0, 0, 0 };
+                AddState();
+            }
+
+            public void Next()
+            {
+                t += dt;
+                while (time[time.Count - 1] <= t)
+                {
+                    AddState();
+                }
+            }
+
+            private void AddState()
+            {
+                int currentState = (int)stat[stat.Count - 1];
+                double tau = Math.Log(rand.NextDouble()) / Q[currentState][0];
+                state_time[currentState] += tau;
+                time.Add(time[time.Count - 1] + tau);
+                stat.Add(GenerateState(Q[currentState]));
+            }
+
+            private Weather GenerateState(double[] prob)
+            {
+                var value = rand.NextDouble(); int i = 1;
+                while (prob[i] <= value)
+                {
+                    value -= prob[i++];
+                }
+
+                return (Weather)(i - 1);
+            }
+        }
+        private Model model;
+        public enum Weather
+        {
+            clear = 0, cloudy = 1, rainy = 2
+        }
+
+        private void InitFormAndModel()
+        {
+            model.Init();
+            UpdateForm();
+        }
+
+        private void UpdateForm()
+        {
+            var time = (int)Math.Ceiling(model.t * 10);
+
+            lbWeather.Text = GetLabel(model.stat[model.stat.Count - 2]);
+            lbDay.Text = $"{time / 100}.{time % 100}";
+            lbClear.Text = (100 * model.state_time[0] / model.time[model.time.Count - 1]).ToString("f1");
+            lbCloudy.Text = (100 * model.state_time[1] / model.time[model.time.Count - 1]).ToString("f1");
+            lbRainy.Text = (100 * model.state_time[2] / model.time[model.time.Count - 1]).ToString("f1");
+        }
+
+        private string GetLabel(Weather state)
+        {
+            switch (state)
+            {
+                case Weather.clear: return "Ясно :)";
+                case Weather.cloudy: return "Облачно :|";
+                case Weather.rainy: return "Дождь :(";
+            }
+            return "";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            model.Next();
+            UpdateForm();
+        }
+    }
+}
